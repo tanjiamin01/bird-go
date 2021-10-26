778cb574317faa0f4f214e85a0f8e137d926114a
Bryan Ong <bryanongse@gmail.com>
2021-10-26 19:37:42 +0800

    [Feature] Added firestore connection to frontend - Jared/JiaMin

--- App/lib/widget_copy/textfield_general_widget.dart
+++ App/lib/widget_copy/textfield_general_widget.dart
@@ -1,7 +1,14 @@
+
+import 'dart:io';
+
+import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:googlemapstry/widget_copy/button_widget.dart';
-
-
+import'package:firebase_storage/firebase_storage.dart';
+import'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 import '../location copy.dart';
 import '../photo_copy.dart';
 
@@ -13,10 +20,16 @@ class TextfieldGeneralWidget extends StatefulWidget {
 
 class _TextfieldGeneralWidgetState extends State<TextfieldGeneralWidget> {
   bool pressGeoON = false;
-  final emailController = TextEditingController();
+  final NameController = TextEditingController();
+  final DescriptionController = TextEditingController();
   final numberController = TextEditingController();
   String password = '';
   bool isPasswordVisible = false;
+  Location location = new Location();
+  var position;
+  String url = "";
+  CollectionReference _birdss =
+  FirebaseFirestore.instance.collection('birds');
 
   // get key => null; // added this to fix button widget below
 
@@ -24,12 +37,12 @@ class _TextfieldGeneralWidgetState extends State<TextfieldGeneralWidget> {
   void initState() {
     super.initState();
 
-    emailController.addListener(() => setState(() {}));
+    NameController.addListener(() => setState(() {}));
   }
-  
+
   @override
   void dispose() {
-    emailController.dispose();
+    NameController.dispose();
     numberController.dispose();
     super.dispose();
   }
@@ -42,144 +55,151 @@ class _TextfieldGeneralWidgetState extends State<TextfieldGeneralWidget> {
 
         image: DecorationImage(
 
-            fit:BoxFit.fill,
+          fit:BoxFit.fill,
           image: AssetImage('assets/canva-photo-editor.png'),
-           // image: AssetImage('assets/uploadbg1.png'),
-           // image: AssetImage('assets/uploadbg2.png'),
+          // image: AssetImage('assets/uploadbg1.png'),
+          // image: AssetImage('assets/uploadbg2.png'),
         ),
-        ),
+      ),
 
 
-          child: Center( child: ListView(
-            padding: EdgeInsets.all(32),
-            children: [
-              IconButton(
-                alignment: Alignment.topLeft,
-                color: Colors.white,
-                icon: const Icon(Icons.arrow_back_ios_new_rounded, size: 35.0),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Text("UPLOAD YOUR SIGHTING",
-                style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w800,
+      child: Center( child: ListView(
+        padding: EdgeInsets.all(32),
+        children: [
+          IconButton(
+            alignment: Alignment.topLeft,
+            color: Colors.white,
+            icon: const Icon(Icons.arrow_back_ios_new_rounded, size: 35.0),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          Text("UPLOAD YOUR SIGHTING",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.w800,
 
-              ),  ),
-              const SizedBox(height: 30),
-              buildEmail(),
-              const SizedBox(height: 24),
-              description(),
-              const SizedBox(height: 24),
-              // buildNumber(),
-              // const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xffFEAA9C),
-                      onPrimary: Colors.white,
-                      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-                      textStyle: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
+            ),  ),
+          const SizedBox(height: 30),
+          buildEmail(),
+          const SizedBox(height: 24),
+          description(),
+          const SizedBox(height: 24),
+          // buildNumber(),
+          // const SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xffFEAA9C),
+                  onPrimary: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
 
-                      ),
-                    ),
-                    onPressed:() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => UploadImageDemo()),
-                      );
-                    },
-                    icon: Icon(Icons.add_photo_alternate , size: 24),
-                    label: Text("Photo"),
                   ),
-                  ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      primary: /* pressGeoON == true?*/ Color(0xffFEAA9C), // : Color(0xff75E6E7),
-                      onPrimary: Colors.white,
-                      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-                      textStyle: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    onPressed:() {
-                      /*setState(() {
-                        pressGeoON = !pressGeoON;
-                      });*/
-                      Navigator.push(
+                ),
+                onPressed:() async{
+                  await uploadImage();
+                  /* Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => UploadImageDemo()),*/
+                },
+                icon: Icon(Icons.add_photo_alternate , size: 24),
+                label: Text("Photo"),
+              ),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: /* pressGeoON == true?*/ Color(0xffFEAA9C), // : Color(0xff75E6E7),
+                  onPrimary: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed:() async{
+                  var pos = await location.getLocation();
+                  setState(() {
+                    position = pos;
+                  });
+                  /*Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => GeoListenPage()),
-                      );
-                    },
-                    icon: /*pressGeoON == true?*/ Icon(Icons.add_location_alt_rounded, size: 24), //: Icon(Icons.check_circle_outline_rounded, size: 24),
-                    label: /* pressGeoON ==true?*/  Text("Upload Location"),// : Text("Upload Done"),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 24),
-              ButtonWidget(
-                text: 'Submit',
-                onClicked: () {
-                  print('Email: ${emailController.text}');
-                  print('Password: ${password}');
-                  print('Number: ${numberController.text}');
+                      );*/
                 },
+                icon: /*pressGeoON == true?*/ Icon(Icons.add_location_alt_rounded, size: 24), //: Icon(Icons.check_circle_outline_rounded, size: 24),
+                label: /* pressGeoON ==true?*/  Text("Upload Location"),// : Text("Upload Done"),
               ),
             ],
           ),
+          const SizedBox(height: 24),
+          ButtonWidget(
+            text: 'Submit',
+            onClicked: () async{
+              final String? name = NameController.text;
+              final String? description = DescriptionController.text;
+              if (name != null && position != null && url != null){
+                //var pos = await location.getLocation();
+                await _birdss.add({"name": NameController.text, "timestamp": Timestamp.now(), "lat": position.latitude, 'lng': position.longitude, 'imgurl': url, 'description': description});
+              }
+              print('Email: ${NameController.text}');
+              //print('Password: ${password}');
+              print('Description: ${DescriptionController.text}');
+            },
           ),
-        ),
+        ],
+      ),
+      ),
+    ),
   );
 
   Widget buildEmail() => TextField(
-        // controller: emailController,
-        decoration: InputDecoration(
-          enabledBorder: const OutlineInputBorder(
-            // width: 0.0 produces a thin "hairline" border
-            borderSide: const BorderSide(color: Colors.black38, width: 1.0),
-          ),
-
-         // hintText: 'name@example.com',
-          labelText: 'Name of Bird Species',
-          labelStyle: TextStyle(
-          // fontWeight: FontWeight.bold,
-          color: Color(0xffFEAA9C)),
-          filled: true,
-          fillColor: Colors.white,
-          // prefixIcon: Icon(Icons.mail),
-          // icon: Icon(Icons.mail),
-        //  suffixIcon: emailController.text.isEmpty
-          //    ? Container(width: 0)
-         //     : IconButton(
-          //        icon: Icon(Icons.close),
-            //      onPressed: () => emailController.clear(),
-             //   ),
-
-          border: OutlineInputBorder(),
-        ),
-        keyboardType: TextInputType.text,
-        textInputAction: TextInputAction.done,
-
-       );
-    // autofocus: true,
-
-
-  Widget description() => TextField(
-    // controller: emailController,
+    controller: NameController,
     decoration: InputDecoration(
       enabledBorder: const OutlineInputBorder(
         // width: 0.0 produces a thin "hairline" border
         borderSide: const BorderSide(color: Colors.black38, width: 1.0),
       ),
+
       // hintText: 'name@example.com',
+      labelText: 'Name of Bird Species',
+      labelStyle: TextStyle(
+        // fontWeight: FontWeight.bold,
+          color: Color(0xffFEAA9C)),
+      filled: true,
+      fillColor: Colors.white,
+      // prefixIcon: Icon(Icons.mail),
+      // icon: Icon(Icons.mail),
+      //  suffixIcon: emailController.text.isEmpty
+      //    ? Container(width: 0)
+      //     : IconButton(
+      //        icon: Icon(Icons.close),
+      //      onPressed: () => emailController.clear(),
+      //   ),
+
+      border: OutlineInputBorder(),
+    ),
+    keyboardType: TextInputType.text,
+    textInputAction: TextInputAction.done,
+
+  );
+  // autofocus: true,
+
+
+  Widget description() => TextField(
+    controller: DescriptionController,
+    decoration: InputDecoration(
+      enabledBorder: const OutlineInputBorder(
+        // width: 0.0 produces a thin "hairline" border
+        borderSide: const BorderSide(color: Colors.black38, width: 1.0),
+      ),
+      hintText: 'Example: Corner of Carpark C, on highest branch of tallest tree.',
       labelText: 'Specific Description of Location',
       labelStyle: TextStyle(
         // fontWeight: FontWeight.bold,
@@ -239,4 +259,27 @@ class _TextfieldGeneralWidgetState extends State<TextfieldGeneralWidget> {
           ),
         ],
       );*/
+  uploadImage() async{
+    final _storage = FirebaseStorage.instance;
+    final _picker = ImagePicker();
+
+    //Select Image
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    var file = File(image!.path);
+
+    if (image != null){
+      var snapshot = await _storage.ref().child(image.name).putFile(file);
+
+      var downloadUrl = await snapshot.ref.getDownloadURL();
+
+      setState(() {
+        url = downloadUrl;
+      });
+    } else{
+      print("NO IMAGE RECEIVED");
+    }
+
+
+    //Upload to Firebase
+  }
 }
